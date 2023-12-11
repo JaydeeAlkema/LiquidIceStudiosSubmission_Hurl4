a4b9e06ee3f8bb95fbc8639b512bf74e787bccc4
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2023-12-11 13:08:07 +0100

    Add "DiscThrower" class

---
+++ Assets/Scripts/DiscThrower.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+	/// <summary>
+	/// The Disc Thrower class is responsible for keeping track which players turn it currently it is, and, ofcourse, handling the throwing part.
+	/// </summary>
+	public class DiscThrower : MonoBehaviour
+	{
+		
+	}
+}
