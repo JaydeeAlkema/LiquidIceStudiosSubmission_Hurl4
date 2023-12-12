d22192da22b73ef0cc05630b4daf22a30173e52a
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2023-12-12 11:25:16 +0100

    Cleanup variables. Add throwing functionality

--- Assets/Scripts/DiscThrower.cs
+++ Assets/Scripts/DiscThrower.cs
@@ -1,5 +1,6 @@
 ﻿using NaughtyAttributes;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,28 +11,33 @@ namespace Assets.Scripts
 	/// </summary>
 	public class DiscThrower : MonoBehaviour
 	{
-		[SerializeField, BoxGroup("Runtime")] private int currentPlayer = 0; // 0 means nobody is playing. Basically a fail-safe
 
 		[SerializeField, BoxGroup("Settings")] private AnimationCurve throwPowerCurve;
+		[SerializeField, BoxGroup("Settings")] private float throwPowerMultiplier = 15;
 		[SerializeField, BoxGroup("Settings")] private float rotationSpeed = 90f;
 
-		[SerializeField, BoxGroup("References")] private GameObject playerOneDiscPrefab;
-		[SerializeField, BoxGroup("References")] private GameObject playerTwoDiscPrefab;
+		[SerializeField, BoxGroup("References")] private List<GameObject> playerOneDiscs = new List<GameObject>();
+		[SerializeField, BoxGroup("References")] private List<GameObject> playerTwoDiscs = new List<GameObject>();
 		[Space]
 		[SerializeField, BoxGroup("References")] private Transform playerOneStartPosition;
 		[SerializeField, BoxGroup("References")] private GameObject playerOneStartPositionPowerIndicator;
 		[SerializeField, BoxGroup("References")] private Transform playerTwoStartPosition;
 		[SerializeField, BoxGroup("References")] private GameObject playerTwoStartPositionPowerIndicator;
 
-
-		private PlayerInputs inputs = default;
-		private Transform currentSpawnPoint = null;
-		private GameObject currentPowerIndicator = null;
-		private GameObject currentDisc = null;
-		private float rot = 0;
-		private bool isPoweringUp = false;
-		float poweringUpTime = 0;
-		float powerCurveValue = 0;
+		[SerializeField, ReadOnly, BoxGroup("Privates")] private int currentPlayer = 0; // 0 means nobody is playing. Basically a fail-safe
+		[SerializeField, ReadOnly, BoxGroup("Privates")] private int playerOneIndex = 0;
+		[SerializeField, ReadOnly, BoxGroup("Privates")] private int playerTwoIndex = 0;
+		[Space]
+		[SerializeField, ReadOnly, BoxGroup("Privates")] private PlayerInputs inputs = default;
+		[SerializeField, ReadOnly, BoxGroup("Privates")] private Transform currentSpawnPoint = null;
+		[SerializeField, ReadOnly, BoxGroup("Privates")] private GameObject currentPowerIndicator = null;
+		[Space]
+		[SerializeField, ReadOnly, BoxGroup("Privates")] private float rot = 0;
+		[Space]
+		[SerializeField, ReadOnly, BoxGroup("Privates")] private bool isPoweringUp = false;
+		[SerializeField, ReadOnly, BoxGroup("Privates")] private bool hasReleasedDisc = true;
+		[SerializeField, ReadOnly, BoxGroup("Privates")] private float poweringUpTime = 0;
+		[SerializeField, ReadOnly, BoxGroup("Privates")] private float powerCurveValue = 0;
 
 		private void Awake()
 		{
@@ -50,8 +56,8 @@ namespace Assets.Scripts
 
 		private void Update()
 		{
-			ReadPlayerInputs();
-			GetPower();
+			ReadDirectionalInputs();
+			PowerUpDisc();
 		}
 
 		private void OnDisable()
@@ -64,7 +70,6 @@ namespace Assets.Scripts
 			if (currentPlayer == 0) currentPlayer = Random.Range(1, 3);
 			else currentPlayer = currentPlayer == 1 ? 2 : 1;
 
-			currentDisc = currentPlayer == 1 ? playerOneDiscPrefab : playerTwoDiscPrefab;
 			if (currentPlayer == 1)
 			{
 				playerOneStartPosition.gameObject.SetActive(true);
@@ -81,10 +86,11 @@ namespace Assets.Scripts
 			}
 		}
 
-		private void ReadPlayerInputs()
+		private void ReadDirectionalInputs()
 		{
 			float directionInput = currentPlayer == 1 ? inputs.Player.PlayerOne_DirectionKeys.ReadValue<float>() : inputs.Player.PlayerTwo_DirectionKeys.ReadValue<float>();
 			if (directionInput == 0) return;
+
 
 			float rotationChange = -directionInput * rotationSpeed * Time.deltaTime;
 			rot += rotationChange;
@@ -92,7 +98,7 @@ namespace Assets.Scripts
 			currentSpawnPoint.rotation = Quaternion.Euler(0, 0, rot);
 		}
 
-		private void GetPower()
+		private void PowerUpDisc()
 		{
 			float powerInput = currentPlayer == 1 ? inputs.Player.PlayerOne_PowerInput.ReadValue<float>() : inputs.Player.PlayerTwo_PowerInput.ReadValue<float>();
 			isPoweringUp = powerInput != 0;
@@ -101,18 +107,43 @@ namespace Assets.Scripts
 			{
 				poweringUpTime += Time.deltaTime;
 				powerCurveValue = throwPowerCurve.Evaluate(poweringUpTime);
-				//currentPowerIndicator.transform.localScale = new Vector3(1, currentPowerIndicator.transform.localScale.y + powerCurveValue, 1);
-				Debug.Log(powerCurveValue);
-			}
-			else
-			{
-				currentPowerIndicator.transform.localScale = new Vector3(1, 1, 1f);
-				powerCurveValue = throwPowerCurve.Evaluate(poweringUpTime);
-				Debug.Log($"Released power at: {powerCurveValue}");
-				powerCurveValue = 0;
-				poweringUpTime = 0f;
-			}
+				currentPowerIndicator.transform.localScale = new Vector3(1, 1 + (powerCurveValue * 3), 1);
+				//Debug.Log(powerCurveValue);
 
+				hasReleasedDisc = false;
+			}
+			if (!isPoweringUp && !hasReleasedDisc)
+			{
+				ReleaseDiscWithPower();
+			}
+		}
+
+		private void ReleaseDiscWithPower()
+		{
+
+			powerCurveValue = throwPowerCurve.Evaluate(poweringUpTime);
+			Debug.Log($"Released power at: {powerCurveValue}");
+
+			GameObject discToThrow = currentPlayer == 1 ? playerOneDiscs[playerOneIndex] : playerTwoDiscs[playerTwoIndex];
+			discToThrow.gameObject.SetActive(true);
+			float angle = currentSpawnPoint.transform.eulerAngles.z;
+			angle += 90;
+			float angleRad = angle * Mathf.Deg2Rad;
+			Vector2 throwDirection = new(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+			discToThrow.GetComponent<Rigidbody2D>().velocity = throwDirection * powerCurveValue * throwPowerMultiplier;
+
+			currentPowerIndicator.transform.localScale = new Vector3(1, 1, 1);
+			currentSpawnPoint.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+			powerCurveValue = 0;
+			poweringUpTime = 0f;
+			rot = 0;
+
+			if (currentPlayer == 1) playerOneIndex++;
+			else if (currentPlayer == 2) playerTwoIndex++;
+
+			hasReleasedDisc = true;
+			GameLoop();
 		}
 
 		private void EnableInputs()
