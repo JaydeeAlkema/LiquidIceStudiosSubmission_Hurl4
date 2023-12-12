b306c88ff7370e413c997199fab9772de8387273
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2023-12-12 15:19:32 +0100

    Add Scriptable Int to avoid having unnecessary class references.

---
+++ Assets/Scripts/Scriptables/ScriptableInt.cs
@@ -0,0 +1,27 @@
+﻿using NaughtyAttributes;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Scriptables
+{
+	[CreateAssetMenu(fileName = "Scriptable Int", menuName = "ScriptableObjects/New Scriptable Int")]
+	public class ScriptableInt : ScriptableObject
+	{
+		[SerializeField] private int value;
+		[SerializeField] private bool shouldReset = false;
+		[SerializeField, ShowIf("shouldReset")] private int startValue;
+
+		public int Value { get => value; set => this.value = value; }
+
+
+		private void OnDisable()
+		{
+			if (shouldReset) value = startValue;
+		}
+
+		private void OnDestroy()
+		{
+			if (shouldReset) value = startValue;
+		}
+	}
+}
