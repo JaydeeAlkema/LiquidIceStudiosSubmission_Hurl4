0d19ed7e482506ab964783813de19176f6fbdc40
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2023-12-12 15:26:02 +0100

    Add Scriptable Bool

---
+++ Assets/Scripts/Scriptables/ScriptableBool.cs
@@ -0,0 +1,26 @@
+﻿using NaughtyAttributes;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Scriptables
+{
+	[CreateAssetMenu(fileName = "Scriptable Bool", menuName = "ScriptableObjects/New Scriptable Bool")]
+	public class ScriptableBool : ScriptableObject
+	{
+		[SerializeField] private bool value;
+		[SerializeField] private bool shouldReset = false;
+		[SerializeField, ShowIf("shouldReset")] private bool startValue;
+
+		public bool Value { get => value; set => this.value = value; }
+
+		private void OnDisable()
+		{
+			if (shouldReset) value = startValue;
+		}
+
+		private void OnDestroy()
+		{
+			if (shouldReset) value = startValue;
+		}
+	}
+}
