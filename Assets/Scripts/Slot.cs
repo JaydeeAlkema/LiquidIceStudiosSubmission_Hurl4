d3c25adec07b9ec056d4172a7feefec60c45fc2b
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2023-12-12 12:47:31 +0100

    Add slot prefab. Add functionality to Slot class

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -6,6 +6,44 @@ namespace Assets.Scripts
 {
 	public class Slot : MonoBehaviour
 	{
+		[SerializeField] private Playfield playfield = null;
+		[SerializeField] private int player = 0;
 
+		private GameObject discInSlot = null;
+		private Coroutine coroutine = null;
+
+		void SetPlayfield(Playfield playfield)
+		{
+			this.playfield = playfield;
+		}
+
+		private void OnTriggerEnter2D(Collider2D collision)
+		{
+			if (collision.name.Contains("Disc"))
+			{
+				Debug.Log($"{collision.name} entered {name}", this);
+				discInSlot = collision.gameObject;
+				coroutine = StartCoroutine(CountdownTillDiscIsStableInSlot());
+			}
+		}
+		private void OnTriggerExit2D(Collider2D collision)
+		{
+			if (collision.name.Contains("Disc"))
+			{
+				Debug.Log($"{collision.name} exited {name}", this);
+				discInSlot = null;
+				StopCoroutine(coroutine);
+			}
+		}
+
+		private IEnumerator CountdownTillDiscIsStableInSlot()
+		{
+			yield return new WaitForSeconds(1);
+			if (discInSlot == null) yield return null;
+			if (discInSlot.GetComponentInParent<Rigidbody2D>().velocity != Vector2.zero) yield return null;
+
+			player = discInSlot.name.Contains("P1") ? 1 : 2;
+			Debug.Log($"<color=green>{discInSlot.name} now belongs to {name}</color>", this);
+		}
 	}
 }
